e2f062b82bedd5f346402221cb3f4dbc7fdfee95
Thuyet Y <ykimcnc@gmail.com>
2020-11-27 18:44:40 +0700

    reinstall package

---
+++ OnlineMoviesBooking/Controllers/DiscountsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineMoviesBooking.Models.Models;
+
+namespace OnlineMoviesBooking.Controllers
+{
+    public class DiscountsController : Controller
+    {
+        private readonly CinemaContext _context;
+
+        public DiscountsController(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Discounts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Discount.ToListAsync());
+        }
+
+        // GET: Discounts/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.Discount
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return View(discount);
+        }
+
+        // GET: Discounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Discounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,PercentDiscount,MaxCost,DateStart,DateEnd,ImageDiscount")] Discount discount)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(discount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(discount);
+        }
+
+        // GET: Discounts/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.Discount.FindAsync(id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+            return View(discount);
+        }
+
+        // POST: Discounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Description,PercentDiscount,MaxCost,DateStart,DateEnd,ImageDiscount")] Discount discount)
+        {
+            if (id != discount.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(discount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DiscountExists(discount.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(discount);
+        }
+
+        // GET: Discounts/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.Discount
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return View(discount);
+        }
+
+        // POST: Discounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var discount = await _context.Discount.FindAsync(id);
+            _context.Discount.Remove(discount);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DiscountExists(string id)
+        {
+            return _context.Discount.Any(e => e.Id == id);
+        }
+    }
+}
