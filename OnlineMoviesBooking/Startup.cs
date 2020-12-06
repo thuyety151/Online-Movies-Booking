7c66098101922d36d756d3f7e6dff8bd767c87ed
Thuyet Y <ykimcnc@gmail.com>
2020-12-06 21:16:50 +0700

    Add Admin area

--- OnlineMoviesBooking/Startup.cs
+++ OnlineMoviesBooking/Startup.cs
@@ -67,6 +67,10 @@ namespace OnlineMoviesBooking
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapAreaControllerRoute(
+                     name: "Admin",
+                     areaName: "Admin",
+                     pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
